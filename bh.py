43cc867e3cf5e1919b5d47e1540167e46e9228f6
Yu Gai <yg1246@nyu.edu>
2019-04-16 08:00:00 +0800

    commit

--- bh.py
+++ bh.py
@@ -13,13 +13,14 @@ A = mat['network']
 n = A.shape[0]
 D = sps.diags(A.tocsr().dot(np.ones(n)))
 y = mat['group'].todense()
-r2 = sp.mean(D)
+r2 = sp.sum(D) / n
 Hp = (r2 - 1) * sps.eye(n) - r2 ** 0.5 * A + D
 Hm = (r2 - 1) * sps.eye(n) + r2 ** 0.5 * A + D
-wp, vp = linalg.eigsh(Hp, k=16, which='SA')
+wp, vp = linalg.eigsh(Hp, k=2, which='SA')
 print(sorted(wp))
-wm, vm = linalg.eigsh(Hm, k=16, which='SA')
+wm, vm = linalg.eigsh(Hm, k=2, which='SA')
 print(sorted(wm))
-_, y_bar = kmeans2(np.vstack([vp, vm]), y.shape[1])
+_, y_bar = kmeans2(np.hstack([vp, vm]), y.shape[1])
+print(y.shape, y_bar.shape)
 print(f1_score(np.argmax(y, axis=1), y_bar, average='micro'))
 print(f1_score(np.argmax(y, axis=1), y_bar, average='macro'))
